f26438a9350bd9a1c04bba1996f12b0aa6fce820
shellawa <awasymmetry@gmail.com>
2024-12-29 00:11:55 +0700

    feat: use giscus for comment section

--- src/routes/blogs/[id]/+page.svelte
+++ src/routes/blogs/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Carta, Markdown } from "carta-md"
   import DOMPurify from "isomorphic-dompurify"
+  import Giscus from "@giscus/svelte"
   import { code } from "@cartamd/plugin-code"
   import "$lib/styles/markdown.pcss"
 
@@ -15,5 +16,21 @@
 </script>
 
 <div class="my-6 flex justify-around px-4">
-  <div class="markdown min-w-0 max-w-5xl"><Markdown {carta} {value} /></div>
+  <div class="markdown min-w-0 max-w-5xl">
+    <Markdown {carta} {value} />
+    <Giscus
+      repo="shellawa/shellawa.github.io"
+      repoId="MDEwOlJlcG9zaXRvcnkzNzU1NjEwMTg="
+      category="Announcements"
+      categoryId="DIC_kwDOFmKbOs4ClkvT"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="transparent_dark"
+      lang="en"
+      loading="lazy"
+    />
+  </div>
 </div>
