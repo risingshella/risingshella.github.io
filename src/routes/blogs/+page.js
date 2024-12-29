b32a0f3eb970479669ac74509c1b202bc844c316
shellawa <awasymmetry@gmail.com>
2024-12-29 19:34:09 +0700

    feat: correctly return blogs map

--- src/routes/blogs/+page.js
+++ src/routes/blogs/+page.js
@@ -3,5 +3,5 @@ import { PUBLIC_CONTENT_ORIGIN } from "$env/static/public"
 export async function load({ fetch }) {
   const res = await fetch(`${PUBLIC_CONTENT_ORIGIN}/blogs/map.json`)
   const blogs_map = await res.json()
-  return { blogs_map: Array(12).fill(blogs_map[0]) }
+  return { blogs_map }
 }
